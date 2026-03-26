20a8dcd57a505cca7ef7b2895598bb47fdc0ffda
Deepfake Detection <user@example.com>
2026-03-26 09:56:44 +0530

    Temp: Delete all tracked files from GitHub

--- train_fft.py
+++
@@ -1,9 +0,0 @@
-from src.models.fft_analysis import train_fft_model
-
-train_fft_model(
-    real_dir  = "data/images/real",
-    fake_dir  = "data/images/fake",
-    save_path = "models/fft_classifier.pth",
-    epochs    = 30,
-    batch_size= 64,
-)
